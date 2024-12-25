cdec7dac6deddf1b2739114ed1258a4a1c11fa12
molkify 3 <robertbarcelay@gmail.com>
2024-12-24 21:57:25 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/uv/service/',
-    bare: 'https://bare.benrogo.net/',
+    bare: 'https://szvy.gay/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
