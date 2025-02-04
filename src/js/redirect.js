46f185b78d7318531f200aabff35a3bdbd3b4c0d
m... <robertbarcelay@gmail.com>
2025-02-03 19:51:13 -0500

    oh fuckk

--- src/js/redirect.js
+++ src/js/redirect.js
@@ -2,7 +2,7 @@ document.addEventListener('keydown', function(event) {
 
     if (event.key === '8') {
   
-      window.location.href = "https://www.example.com"; 
+      window.location.href = "/static/index.html"; 
   
     }
   
