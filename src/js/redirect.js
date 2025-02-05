d10ae9ee40a4957468861853f03bd315cd241794
m... <robertbarcelay@gmail.com>
2025-02-04 22:59:16 -0500

    fuck

--- src/js/redirect.js
+++ src/js/redirect.js
@@ -2,7 +2,7 @@ document.addEventListener('keydown', function(event) {
 
     if (event.key === '8') {
   
-      window.location.href = "/static/index.html"; 
+      window.location.href = "/s/index.html"; 
   
     }
   
