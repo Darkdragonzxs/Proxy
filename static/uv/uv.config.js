a191ed8eb593206cfab1230bb883e3728a741f8c
m... <robertbarcelay@gmail.com>
2025-02-04 22:51:46 -0500

    the story on how forgetting one slash nearly ended a project

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/~/',
-    bare:'https://bare.benrogo.net',
+    bare:'https://bare.benrogo.net/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
