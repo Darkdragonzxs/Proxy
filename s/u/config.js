5973d027b87df5d74a9782111c591be95420a97b
m... <robertbarcelay@gmail.com>
2025-02-21 22:28:56 -0500

    selfhosted bare server and css changes

--- s/u/config.js
+++ s/u/config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/s/go/',
-    bare:'https://bare.benrogo.net/',
+    bare:'https://t.thecappuccino.site/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/s/u/handler.js',
